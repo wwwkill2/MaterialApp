778b2356108893fb5c60dbb3935b6458ff8d9a56
wangjiaming <1260101013@qq.com>
2017-01-04 17:58:29 +0800

    edit FragmentDrawer

--- app/src/main/java/com/example/wjm/materialapp/activity/FragmentDrawer.java
+++ app/src/main/java/com/example/wjm/materialapp/activity/FragmentDrawer.java
@@ -2,17 +2,22 @@ package com.example.wjm.materialapp.activity;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.example.wjm.materialapp.R;
 import com.example.wjm.materialapp.adapter.NavigationDrawerAdapter;
+import com.example.wjm.materialapp.model.NavDrawerItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,19 +30,53 @@ public class FragmentDrawer extends Fragment {
     private ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
     private NavigationDrawerAdapter mAdapter;
+    private View containerView;
+    private static String[] titles = null;
+    private FragmentDrawerListener mListener;
+
 
 
     public FragmentDrawer() {
         // Required empty public constructor
     }
 
+    public interface FragmentDrawerListener {
+        void onDrawerItemSelected(View view, int position);
+    }
+
+    public void setDrawerListener(FragmentDrawerListener listener) {
+        mListener = listener;
+    }
+
+    public static List<NavDrawerItem> getData() {
+        List<NavDrawerItem> data = new ArrayList<>();
+
+        for(int i = 0; i < titles.length; i++) {
+            NavDrawerItem item = new NavDrawerItem();
+            item.setTitle(titles[i]);
+            data.add(item);
+        }
+
+        return data;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        titles = getActivity().getResources().getStringArray(R.array.nav_drawer_labels);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+        View layout = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_navigation_drawer, container, false);
+        mRecyclerView = (RecyclerView) layout.findViewById(R.id.drawerList);
+        mAdapter = new NavigationDrawerAdapter(getActivity(), getData());
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        
+
+        return layout;
     }
 
 }
